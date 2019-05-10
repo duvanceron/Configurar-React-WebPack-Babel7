56fe85527dab6c31a9e5b1cb49a3423dc68fa1b5
duvanceron <duvanceroon@hotmail.com>
2019-05-09 23:06:16 -0500

    Configurar react

---
+++ webpack-react/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry:'./src/app/index.js',
+    output:{
+        path:__dirname+'/src/public',
+        filename:'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+      }
+  };
